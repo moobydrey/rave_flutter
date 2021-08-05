37e3ecadcc2ea0ae2c2b032c05ff0ec33e0cdc1a
Alabidun Mubarak Oluwadamilare <oluwadamilaremubarak@gmail.com>
2021-08-05 22:28:33 +0100

    fixed a bug

--- lib/src/dto/charge_request_body.dart
+++ lib/src/dto/charge_request_body.dart
@@ -19,7 +19,7 @@ class ChargeRequestBody extends Equatable {
   ChargeRequestBody.fromPayload({Payload? payload, String? type})
       : this.pBFPubKey = payload!.pbfPubKey,
         this.alg = "3DES-24",
-        this.client = getEncryptedData(json.encode(payload.toJson(type ?? '')),
+        this.client = getEncryptedData(json.encode(payload.toJson(type!)),
             Repository.instance.initializer.encryptionKey);
 
   Map<String, dynamic> toJson() => {
